93fcaa015686f40089a4b99ae0d1762cff0788da
Michael Sánchez Soto <sanchez.soto.michael@gmail.com>
2025-05-14 18:00:26 -0600

    feat: including new file from week number 9
    This is the file that it was created for the practice that we were working on the first class from the course called "Programación para Estadística 1".

---
+++ Semana 09.R
@@ -0,0 +1,26 @@
+library(dplyr)
+
+# Clientes
+clientes = data.frame(
+  id_cliente = c(1, 2, 3),
+  nombre = c("Ana", "Luis", "María")
+)
+
+# Pedidos
+pedidos = data.frame(
+  id_cliente = c(2, 3, 4),
+  producto = c("Libro", "Lápiz", "Cuaderno")
+)
+
+# inner_join()
+inner_join(clientes, pedidos, by = "id_cliente")
+
+# left_join()
+left_join(clientes, pedidos, by = "id_cliente")
+
+# right_join()
+right_join(clientes, pedidos, by = "id_cliente")
+
+# full_join()
+full_join(clientes, pedidos, by = "id_cliente")
+
